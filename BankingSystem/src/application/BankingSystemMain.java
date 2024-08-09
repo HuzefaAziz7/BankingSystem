8d976578e02fa5077e623e518c801f4b3889481b
Huzefa Aziz <workhuzefa7@gmail.com>
2024-08-09 21:03:59 +0530

    Add files via upload

--- BankingSystem/src/application/BankingSystemMain.java
+++ BankingSystem/src/application/BankingSystemMain.java
@@ -17,7 +17,7 @@ public class BankingSystemMain extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BankingSystemMain.primaryStage = primaryStage;
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml_Files/MainPage.fxml"));
 			Parent root = loader.load(); 
 			Scene scene = new Scene(root,700,620);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
---
+++ BankingSystem/src/application/MainPageController.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class MainPageController {
+
+    @FXML // Dashboard Pane.
+    private Button BtnDashboard;
+    private AnchorPane DashboardPane;
+    private Label LblDB1;
+    @FXML // Transaction Pane.
+    private AnchorPane TransactionsPane;
+    private Button BtnTransactions;
+    private Label LblTrans1; 
+    
+    @FXML
+    void OpenDashboard(ActionEvent event) {
+		 DashboardPane.toFront();
+		 DashboardPane.setVisible(true);
+	     TransactionsPane.setVisible(false);
+    }
+    
+    
+    
+    @FXML
+    void OpenTransactions(ActionEvent event) {
+		 TransactionsPane.toFront();
+		 TransactionsPane.setVisible(true);
+	     DashboardPane.setVisible(false);
+    }
+
+}
