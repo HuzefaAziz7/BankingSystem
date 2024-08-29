84a30b4d84ca6324bce6a3a75a738abe4d499a97
Huzefa Aziz <workhuzefa7@gmail.com>
2024-08-30 01:34:28 +0530

    Add files via upload

--- BankingSystem/src/application/EmailController.java
+++ BankingSystem/src/application/EmailController.java
@@ -1,13 +1,80 @@
 package application;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class EmailController {
-	
-} // Main Class.
+    public static void main(String[] args) {
+        // sendEmail(to);
+    }
+
+    // This method is used for sending the email.
+    public static void ResetPasswordEmail(String to, int Passkey) {
+        String message = "Hello! This is an automated message from Capital Bank. \n"
+                + "This Email is regarding the resetting of your password \n"
+                + "Kindly Enter this Passkey " + Passkey + " to reset your password. \n "
+                + "Thank You. \n";
+        
+        String subject = "Password Reset";
+        String from = "codehuzefa@gmail.com";
+        
+        // Variable for Gmail.
+        String host = "smtp.gmail.com";
+        
+        // Get the system properties.
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        // Step 1: Create a session.
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "emvx udym icev cgns");
+            }
+        });
+        
+        session.setDebug(false);
+
+        // Step 2: Compose the message.
+        MimeMessage m = new MimeMessage(session);
+        try {
+            // From email.
+            m.setFrom(new InternetAddress(from));
+        
+            // Adding recipient to message.
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            
+            // Adding subject.
+            m.setSubject(subject);
+            
+            // Adding message.
+            m.setText(message);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Step 3: Send the message using Transport class.
+        try {
+            Transport.send(m);
+            System.out.println("Email sent.");
+//            lblConfirmation.setText("Email Sent Successfully. Please check your email to reset password.");
+        } catch (Exception e) {
+        	System.out.println("74");
+//        	lblConfirmation.setText("Failed to send Email. Please try again.");
+        }
+    }
+}
+
+
--- BankingSystem/src/application/PasswordController.java
+++ BankingSystem/src/application/PasswordController.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.sql.SQLException;
+import java.util.Random;
+
+import org.mindrot.jbcrypt.BCrypt;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -9,22 +14,99 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class PasswordController { 
-	
+	@FXML
+	private TextField TxtFieldFPCustomerID;
 	private ImageView BankImage;
 	private Pane FPOPEmailPane, FPOPMobileNumPane;
 	private AnchorPane FPOptionPane, ForgotPwdPane;
 	private Label LblEmailAddressFP, LblFPCustomerID, LblMainFP, LblMobileNumberFP, LblReqEmail, LblReqNumber;
-	private TextField TxtFieldFPCustomerID;
-
-
+	
+	public static int Passkey; 
+	public static String VerifyEmail;
+	public int CustomerID;
+	
+    @FXML
+    void initialize() {
+    	ImpMethods.AdminDBConnection();
+    } // initialize().
+    
     @FXML
     void ReqEmail(MouseEvent event) {
-    	System.out.println("Email Sent");
+//    	String x = new String(TxtFieldFPCustomerID.getText());
+    	CustomerID = Integer.parseInt(TxtFieldFPCustomerID.getText());  
+    	ForgotPassword(CustomerID);
+//    	System.out.println("Email Sent");
     }
 
     @FXML
     void ReqMobileNumber(MouseEvent event) {
+//    	CustomerID = Integer.parseInt(TxtFieldFPCustomerID.getText());  
+    	ImpMethods.AdminDBConnection();
     	System.out.println("OTP Sent");
     }
+    
+    public void ForgotPassword(int CustomerID) {
+    	String DBEmail = null;
+    	try {
+			ImpMethods.PSUpdate = ImpMethods.MyCon.prepareStatement("SELECT Email FROM ClientInfo WHERE CustomerID = ?");
+			ImpMethods.PSUpdate.setInt(1, CustomerID);
+	        ImpMethods.MyRS = ImpMethods.PSUpdate.executeQuery();
+	        if (ImpMethods.MyRS.next()) {
+	            DBEmail = ImpMethods.MyRS.getString("Email");
+	            generatePasskey();
+	            EmailController.ResetPasswordEmail(DBEmail, Passkey);
+	        }
+	        ImpMethods.PSUpdate.close();
+		} catch (SQLException e) {
+			System.out.println("61");
+			e.printStackTrace();
+			System.out.println("Email not sent. Please try again.");
+		}
+    	
+        
+//            GUI.lblConfirmation.setText("Email/Username Incorrect.");
+        
+    } // Forgot Password.
 
+    /* private void EmailVerification(int CustomerID, String Email) {
+        
+    	String DBEmail = null;
+        try {
+            ImpMethods.PSUpdate = ImpMethods.MyCon.prepareStatement("SELECT Email FROM IdInfo WHERE CustomerID = ?");
+            ImpMethods.PSUpdate.setInt(1, CustomerID);
+            ImpMethods.MyRS = ImpMethods.PSUpdate.executeQuery();
+            if (ImpMethods.MyRS.next()) {
+                DBEmail = ImpMethods.MyRS.getString("Email");
+                
+            }
+            ImpMethods.PSUpdate.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // EmailVerification(). */
+    
+    private void generatePasskey() {
+        Random random = new Random();
+        int min = 100000, max = 1000000;
+        Passkey = random.nextInt(max - min + 1) + min;
+    } // generatePasskey()
+    
+    public void ResetPassword(int CustomerID, int PasskeyRP, String NewPassword) {
+        if (Passkey == PasskeyRP) {
+            try {
+                String HashedPassword = BCrypt.hashpw(NewPassword, BCrypt.gensalt());
+                ImpMethods.PSUpdate = ImpMethods.MyCon.prepareStatement("UPDATE ClientInfo SET Password = ? WHERE CustomerID = ?");
+                ImpMethods.PSUpdate.setString(1, HashedPassword);
+                ImpMethods.PSUpdate.setInt(2, CustomerID);
+                int rowsAffected = ImpMethods.PSUpdate.executeUpdate();
+//                GUI.lblConfirmationRP.setText("Password Changed Successfully.");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("104");
+            }
+        } else {
+            System.out.println("Passkey Incorrect.!");
+        }
+    } // ResetPassword()
+    
 } // Main Class.
